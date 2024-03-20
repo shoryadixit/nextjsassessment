9f2883bf64451608230b46197fa8ea885e9cc510
Shorya Dixit <shauryadt123@gmail.com>
2024-03-20 11:15:51 +0530

    Project Setup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
+import React from 'react';
 import "@/styles/globals.css";
+import "tailwindcss/tailwind.css";
+import PropTypes from "prop-types";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+App.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
