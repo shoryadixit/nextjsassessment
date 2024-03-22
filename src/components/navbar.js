29db63b59dc9f4419f1c359ddd6dd17978861b89
Shorya Dixit <shauryadt123@gmail.com>
2024-03-22 16:57:51 +0530

    Navbar Fixes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,23 +7,36 @@ import { get } from 'lodash';
 import { FaChevronDown } from 'react-icons/fa6';
 import { LuMenu } from 'react-icons/lu';
 import Link from 'next/link';
+import BannerImage from '/public/assets/BannerImage.webp';
 
 function Navbar({ NavTabs }) {
   return (
-    <div
-      className='relative flex w-full items-center justify-center'
-      style={{
-        background:
-          'linear-gradient(90.3deg, #2B4D93 -10.68%, #0E205B 27.98%, #162867 55.94%, #323A87 103.52%)',
-      }}
-    >
+    <div className='relative flex w-full items-center justify-center'>
+      <Image
+        src={BannerImage}
+        alt='Banner Image'
+        fill
+        className='object-cover object-top sm:object-center'
+      />
       <div className='flex py-3 px-4 sm:px-10 items-center static z-[1] lg:max-w-[1350px] justify-between w-full gap-20'>
         <div className='relative hidden lg:flex w-[300px] h-14'>
-          <Link href={'/'}><Image src={logo} fill alt='Logo' className='cursor-pointer object-contain' /></Link>
+          <Link href={'/'}>
+            <Image
+              src={logo}
+              fill
+              alt='Logo'
+              className='cursor-pointer object-contain'
+            />
+          </Link>
         </div>
 
         <div className='relative flex lg:hidden w-[35px] sm:w-[50px] h-14'>
-          <Image src={MobileLogo} fill alt='Logo' className='cursor-pointer object-contain' />
+          <Image
+            src={MobileLogo}
+            fill
+            alt='Logo'
+            className='cursor-pointer object-contain'
+          />
         </div>
 
         <div className='hidden lg:flex items-center justify-between w-full gap-6'>
@@ -44,7 +57,7 @@ function Navbar({ NavTabs }) {
               </div>
             ))}
         </div>
-        
+
         <div className='cursor-pointer flex bg-gradient-to-br from-blue-500 via-blue-600 px-5 py-2 rounded-[8px] to-purple-700 w-fit whitespace-nowrap'>
           <button className='font-jost font-[400] text-[14px] sm:text-[20px] text-white'>
             Request An Audit
